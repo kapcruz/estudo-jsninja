6c76559a3ea9fae79f1b6d38ad957525db3279aa
Karina <kapcruz@gmail.com>
2019-12-23 16:55:56 -0300

    desafio 19

--- desafios/desafio19/desafio19.js
+++ desafios/desafio19/desafio19.js
@@ -28,6 +28,9 @@ no console:
 console.log( 'Regex para números usando o construtor:' );
 // ?
 
+var justNumbersRegex = new RegExp( '^\\d+', 'gm' );
+console.log(justNumbersRegex);
+
 /*
 Verifique se a regex acima casa com o texto na variável `text`, mostrando o
 resultado no console. O resultado deve ser:
@@ -36,6 +39,8 @@ resultado no console. O resultado deve ser:
 var text = '10 anos.\n50 discos vendidos.\nE nem 10% dos meus amigos o conhece.';
 console.log( '\nNúmeros no início da linha do texto:\n' + text, '\n' );
 // ?
+
+console.log(text.match(justNumbersRegex));
 
 /*
 - Crie uma regex que case com números no final de uma string. Atribua a
@@ -48,6 +53,9 @@ Mostre a regex no console:
 console.log( '\nRegex para números somente no final das linhas:' );
 // ?
 
+var numbersAtTheEnd = new RegExp('\\d+$', 'gm')
+console.log(numbersAtTheEnd);
+
 /*
 Verifique se a regex acima casa com o texto na variável `otherText`,
 mostrando o resultado no console.
@@ -57,6 +65,8 @@ O resultado deve ser:
 var otherText = 'Silvio Santos, nome artístico de Senor Abravanel (Rio de Janeiro, 12\n de dezembro de 1930), é um apresentador de televisão e empresário brasileiro.\n Proprietário do Grupo Silvio Santos, que inclui empresas como a Liderança\n Capitalização (administradora da loteria Tele Sena), a Jequiti Cosméticos e o\n Sistema Brasileiro de Televisão (mais conhecido como SBT), Silvio Santos possui\n um patrimônio avaliado em aproximadamente 6\n bilhões de reais.';
 console.log( '\nNúmeros no final da linha:\n\n', otherText, '\n' );
 // ?
+
+console.log( otherText.match(numbersAtTheEnd) );
 
 /*
 Vamos criar um método que vai testar se uma classe CSS existe em uma
@@ -83,5 +93,17 @@ para exemplificar.
 */
 var markup = '<main>\n  <div class="container">\n    <span class="text date"></span>\n    <p class=\'excerpt\'></p>\n  </div>\n</main>';
 console.log( '\nQuais classes CSS existem na marcação abaixo?\n\n', markup, '\n' );
+
+function temClasse(markup, classe) {
+	var regex = new RegExp('class=[\'\"].?' + classe + '.*?[\'\"]');
+	return regex.test(markup) + ' para a classe ' + classe;
+}
+
+console.log( temClasse(markup, "container") );
+console.log( temClasse(markup, "text") );
+console.log( temClasse(markup, "date") );
+console.log( temClasse(markup, "excerpt") );
+console.log( temClasse(markup, "main") );
+
 
 })();
